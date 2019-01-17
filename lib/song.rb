e6276792435730d0e0b5fbab082f6b2058591e87
Steve Albonico <salbonico@gmail.com>
2019-01-17 16:17:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,11 @@ def initialize(name, artist, genre)
 @name = name
 @artist = artist
 @genre = genre
+@@count += 1
+if @@artists.include?(name) == false
+  @@artists.push(name)
+  @@artist_count[name] = 1
+
 
 end
 end
